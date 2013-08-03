40e1772cd9c98b173a3ee9ea54084b909d728b8d
William Ehlhardt <williamehlhardt@gmail.com>
2013-08-03 01:34:21 -0500

    Closer to working...

--- tictactoe.go
+++ tictactoe.go
@@ -7,13 +7,13 @@ import (
 type Tile int
 
 const (
-	X tile = iota
-	O tile = iota
-	Blank tile = iota
+	X Tile = iota
+	O Tile = iota
+	Blank Tile = iota
 )
 
 func (t *Tile) Render() string {
-	switch t {
+	switch *t {
 	case X:
 		return "X"
 	case O:
@@ -28,11 +28,11 @@ func (t *Tile) Render() string {
 type Map [3][3]Tile
 
 func (m *Map) Render() string {
-	lines := [](string*)
-	for x = 0; x < 3; x++ {
-		line := [](string*)
-		for y = 0; y < 3; y++ {
-			append(line, &m[x][y].Render())
+	lines := make([]*string, 0)
+	for x := 0; x < 3; x++ {
+		line := make([]*string, 0)
+		for y := 0; y < 3; y++ {
+			append(line, m[x][y].Render())
 		}
 		append(lines, strings.Join(line, "|"))
 	}
