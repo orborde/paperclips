ddd3d22520d0224d85970c7ba4ece5d8d9b3066a
William Ehlhardt <williamehlhardt@gmail.com>
2013-08-03 01:51:29 -0500

    Write a rendertest!

--- tictactoe_test.go
+++ tictactoe_test.go
@@ -3,5 +3,19 @@ package tictactoe
 import "testing"
 
 func TestRender(t *testing.T) {
+	const render = (
+		"X|O| " +
+		"-----" +
+		" | | " +
+		"-----" +
+		"X|X|O")
+	data := Map{
+		{X, O, Blank},
+		{Blank, Blank, Blank},
+		{X, X, O},
+	}
 
+	if data.Render() != render {
+		t.Errorf("Failed to properly render board!")
+	}
 }
