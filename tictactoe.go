0af04816f1f07a351feeaa000bcc04572e997126
William Ehlhardt <williamehlhardt@gmail.com>
2013-08-03 01:35:54 -0500

    MAKE COMPILE

--- tictactoe.go
+++ tictactoe.go
@@ -28,13 +28,13 @@ func (t *Tile) Render() string {
 type Map [3][3]Tile
 
 func (m *Map) Render() string {
-	lines := make([]*string, 0)
+	lines := make([]string, 0)
 	for x := 0; x < 3; x++ {
-		line := make([]*string, 0)
+		line := make([]string, 0)
 		for y := 0; y < 3; y++ {
-			append(line, m[x][y].Render())
+			line = append(line, m[x][y].Render())
 		}
-		append(lines, strings.Join(line, "|"))
+		lines = append(lines, strings.Join(line, "|"))
 	}
-	return strings.Join(line, "-----")
+	return strings.Join(lines, "-----")
 }
