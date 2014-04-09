181e3d5d33631f8596e0f2fb19cd8e8690c93791
William Ehlhardt <williamehlhardt@gmail.com>
2014-04-09 08:56:28 -0500

    Make tests pass

--- paperclips.go
+++ paperclips.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"fmt"
 	"strconv"
-	"strings"
 )
 
 type Move int
@@ -32,28 +31,13 @@ func (b *Board) Apply(move *Move) error {
 	if valid, err := Valid(move, b); !valid {
 		return errors.New(fmt.Sprint("Could not apply move ", move, ":", err.Error()))
 	}
-	b[move.x][move.y] = move.tile
+	b.PaperclipCount -= int(*move)
 	return nil
 }
 
-func (b *Board) Winner() Tile {
-	checkWin := func(x, y []int) Tile {
-
-	}
-}
-
-type Move struct {
-	x, y int
-	tile Tile
-}
-
 func (m *Move) Valid() (bool, error) {
-	if m.x < 0 || m.x > 2 ||
-		m.y < 0 || m.y > 2 {
-		return false, errors.New("Out of bounds")
-	}
-	if m.tile != X && m.tile != O {
-		return false, errors.New("Not a placeable tile")
+	if *m != TakeOne && *m != TakeTwo {
+		return false, errors.New("Not a valid move type")
 	}
 	return true, nil
 }
@@ -62,8 +46,8 @@ func Valid(m *Move, b *Board) (bool, error) {
 	if valid, err := m.Valid(); !valid {
 		return false, err
 	}
-	if b[m.x][m.y] != Blank {
-		return false, errors.New("Tile is occupied")
+	if int(*m) > b.PaperclipCount {
+		return false, errors.New("Not enough paperclips")
 	}
 	return true, nil
 }
--- paperclips_test.go
+++ paperclips_test.go
@@ -3,18 +3,9 @@ package paperclips
 import "testing"
 
 func TestRender(t *testing.T) {
-	const render = ("X|O| " +
-		"-----" +
-		" | | " +
-		"-----" +
-		"X|X|O")
-	data := Board{
-		{X, O, Blank},
-		{Blank, Blank, Blank},
-		{X, X, O},
-	}
-
-	if data.Render() != render {
+	var data Board
+	data.PaperclipCount = 6
+	if data.Render() != "6" {
 		t.Errorf("Failed to properly render board!")
 	}
 }
@@ -33,12 +24,8 @@ func TestMoveBounds(t *testing.T) {
 		}
 	}
 
-	ExpectValid(Move{0, 0, X})
-	ExpectValid(Move{0, 0, O})
-	ExpectInvalid(Move{0, 0, Blank})
-	ExpectInvalid(Move{-1, 0, X})
-	ExpectInvalid(Move{0, -1, X})
-	ExpectInvalid(Move{-1, -1, X})
-	ExpectInvalid(Move{0, 3, X})
-	ExpectInvalid(Move{3, 0, X})
+	ExpectValid(TakeOne)
+	ExpectValid(TakeTwo)
+	ExpectInvalid(3)
+	ExpectInvalid(0)
 }
