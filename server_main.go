d14eb28afda4eea2a078bc96a95bc50b976b4633
William Ehlhardt <williamehlhardt@gmail.com>
2014-04-09 14:37:07 -0500

    Get the RPC server to run

--- server_main.go
+++ server_main.go
@@ -2,11 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net"
+	"net/rpc"
 	"paperclips/paperclips"
 )
 
-var _ paperclips.Board
+const host string = "localhost"
+const port string = "19996"
+
+var address string = host + ":" + port
 
 func main() {
-	fmt.Println("hi there")
+	fmt.Println("Starting up RPC server...")
+	var server paperclips.RPCServer
+	rpc.Register(&server)
+	fmt.Println("Now listening on", address)
+	l, e := net.Listen("tcp", address)
+	if e != nil {
+		log.Fatal("listen error:", e)
+	}
+	rpc.Accept(l)
 }
