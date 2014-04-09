76a702f1261f2b2f75d39fe93aaeab9251ba1f04
William Ehlhardt <williamehlhardt@gmail.com>
2014-04-09 12:28:10 -0500

    Write an RPC wrapper server

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package paperclips
 
 import (
 	"errors"
+	"fmt"
 )
 
 type PlayerID string
@@ -12,12 +13,20 @@ type Server struct {
 	games map[PlayerID]map[BoardID]*Board
 }
 
-func (s *Server) GetGames(P PlayerID) map[BoardID]*Board {
-	return s.games[P]
+func (s *Server) PlayerExists(P PlayerID) bool {
+	_, ret := s.games[P]
+	return ret
+}
+
+func (s *Server) GetGames(P PlayerID) (map[BoardID]*Board, error) {
+	if !s.PlayerExists(P) {
+		return nil, errors.New(fmt.Sprint("Player", P, "does not exist."))
+	}
+	return s.games[P], nil
 }
 
 func (s *Server) MakeMove(player PlayerID, board BoardID, move Move) error {
-	if _, playerExists := s.games[player]; !playerExists {
+	if !s.PlayerExists(player) {
 		return errors.New("Invalid player")
 	}
 
@@ -31,4 +40,25 @@ func (s *Server) MakeMove(player PlayerID, board BoardID, move Move) error {
 	}
 
 	return nil
+}
+
+type RPCServer struct {
+	server Server
+}
+
+func (s *RPCServer) GetGames(P PlayerID, Ret *map[BoardID]*Board) error {
+	var err error
+	*Ret, err = s.server.GetGames(P)
+	return err
+}
+
+type RPCMove struct {
+	Player PlayerID
+	// TODO: check that player is on board
+	BoardID BoardID
+	Move    Move
+}
+
+func (s *RPCServer) MakeMove(Args RPCMove, _ *struct{}) error {
+	return s.server.MakeMove(Args.Player, Args.BoardID, Args.Move)
 }
