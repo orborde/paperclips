590169c03b6a0f361326d106671d156ac5d481f1
William Ehlhardt <williamehlhardt@gmail.com>
2014-04-09 04:54:22 -0500

    move validity checking, v.1

--- tictactoe_test.go
+++ tictactoe_test.go
@@ -19,3 +19,25 @@ func TestRender(t *testing.T) {
 		t.Errorf("Failed to properly render board!")
 	}
 }
+
+func TestMoveBounds(t *testing.T) {
+	ExpectValid := func(m Move) {
+		if !m.Valid() {
+			t.Error("Move", m, "was supposed to be valid!")
+		}
+	}
+	ExpectInvalid := func(m Move) {
+		if m.Valid() {
+			t.Error("Move", m, "was supposed to be invalid!")
+		}
+	}
+
+	ExpectValid(Move{0, 0, X})
+	ExpectValid(Move{0, 0, O})
+	ExpectInvalid(Move{0, 0, Blank})
+	ExpectInvalid(Move{-1, 0, X})
+	ExpectInvalid(Move{0, -1, X})
+	ExpectInvalid(Move{-1, -1, X})
+	ExpectInvalid(Move{0, 3, X})
+	ExpectInvalid(Move{3, 0, X})
+}
