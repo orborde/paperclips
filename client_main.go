09507c74165dfef566842302688895a2d3078985
William Ehlhardt <williamehlhardt@gmail.com>
2014-04-09 18:58:07 -0500

    Download games on client

--- client_main.go
+++ client_main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
+	//"math/rand"
 	"net"
 	"paperclips/paperclips"
 	"strconv"
+	"time"
 )
 
 const host string = "localhost"
@@ -36,5 +38,26 @@ func main() {
 		log.Fatal("Failed to fetch player list:", err)
 	} else {
 		log.Println("Players online are:", names)
+	}
+
+	for {
+		log.Println("Fetching my games...")
+		games, err := client.GetGames(name)
+		if err != nil {
+			log.Fatal("Failed to fetch games:", err)
+		}
+
+		// TODO: Don't have the server dump all the games ever.
+		activeGames := make([]paperclips.BoardID, 0)
+		for id, game := range games {
+			if !game.GameOver() {
+				activeGames = append(activeGames, id)
+			}
+		}
+
+		log.Println("Fetched", len(games), "games, of which", len(activeGames), "are active.")
+
+		log.Println("Time to sleep")
+		time.Sleep(10 * time.Second)
 	}
 }
