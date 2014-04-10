9b4de8aeb200d81727ed83dfcef6bbe645c39491
William Ehlhardt <williamehlhardt@gmail.com>
2014-04-10 01:18:22 -0500

    Move files around more

--- test/test.go
+++ test/test.go
@@ -1,34 +1,8 @@
-package paperclips
+package test
 
 import "testing"
 
-func TestRender(t *testing.T) {
-	var data Board
-	data.PaperclipCount = 6
-	if data.Render() != "6" {
-		t.Errorf("Failed to properly render board!")
-	}
-}
-
-func TestMoveBounds(t *testing.T) {
-	ExpectValid := func(m Move) {
-		if val, err := m.Valid(); !val || !(err == nil) {
-			t.Error(err)
-			t.Error("Move", m, "was supposed to be valid!")
-		}
-	}
-	ExpectInvalid := func(m Move) {
-		if val, err := m.Valid(); val || (err == nil) {
-			t.Error(err)
-			t.Error("Move", m, "was supposed to be invalid!")
-		}
-	}
-
-	ExpectValid(TakeOne)
-	ExpectValid(TakeTwo)
-	ExpectInvalid(3)
-	ExpectInvalid(0)
-}
+//type Adapter interface {
 
 func TestGamePlay(t *testing.T) {
 
