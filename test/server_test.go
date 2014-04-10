e6969bb85b8a98f4f2747e3868bda26694165537
William Ehlhardt <williamehlhardt@gmail.com>
2014-04-10 03:05:06 -0500

    Branch to create server_test

---
+++ test/server_test.go
@@ -0,0 +1,67 @@
+package test
+
+import "testing"
+
+import . "paperclips/paperclips"
+
+import "log"
+
+type RawGameAdapter struct {
+	GameAdapter
+	game        Game
+	firstUpdate *BoardMessage
+}
+
+func NewRawGameAdapter(players []PlayerID, startCount int) GameAdapter {
+	ret := RawGameAdapter{game: *NewGame(players, PaperclipCount(startCount)),
+		firstUpdate: nil}
+	tmp := <-ret.game.FirstUpdate
+	ret.firstUpdate = &tmp
+	return ret
+}
+
+func (a RawGameAdapter) FirstUpdate() BoardMessage {
+	if a.firstUpdate == nil {
+		log.Fatal("IS NIL")
+	}
+	return *(a.firstUpdate)
+}
+
+func (a RawGameAdapter) RunMove(m *Move, p PlayerID, tc TurnCount) (*BoardMessage, error) {
+	result := make(chan MoveResult)
+	a.game.Moves <- MoveMessage{*m, p, tc, result}
+	msg := <-result
+	return msg.BoardMessage, msg.Error
+}
+
+func TestRender(t *testing.T) {
+	var data Board
+	data.PaperclipCount = 6
+	if data.Render() != "6" {
+		t.Errorf("Failed to properly render board!")
+	}
+}
+
+func TestMoveBounds(t *testing.T) {
+	ExpectValid := func(m Move) {
+		if val, err := m.Valid(); !val || !(err == nil) {
+			t.Error(err)
+			t.Error("Move", m, "was supposed to be valid!")
+		}
+	}
+	ExpectInvalid := func(m Move) {
+		if val, err := m.Valid(); val || (err == nil) {
+			t.Error(err)
+			t.Error("Move", m, "was supposed to be invalid!")
+		}
+	}
+
+	ExpectValid(TakeOne)
+	ExpectValid(TakeTwo)
+	ExpectInvalid(3)
+	ExpectInvalid(0)
+}
+
+func TestFoo(t *testing.T) {
+	TestGamePlay(t, NewRawGameAdapter)
+}
